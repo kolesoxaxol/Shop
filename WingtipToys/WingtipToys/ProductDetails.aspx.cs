09584f97212d4caabda9c85dc5fbb7328575b6b3
Yevhenii Koliesnik <dp260793kev@yandex.ua>
2015-12-11 14:35:06 +0200

    Display Data Items and Details

--- WingtipToys/WingtipToys/ProductDetails.aspx.cs
+++ WingtipToys/WingtipToys/ProductDetails.aspx.cs
@@ -2,27 +2,31 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.ModelBinding;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using WingtipToys.Models;
-using System.Web.ModelBinding;
 
 namespace WingtipToys
 {
-    public partial class ProductList : System.Web.UI.Page
+    public partial class ProductDetails : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
 
         }
 
-        public IQueryable<Product> GetProducts([QueryString("id")] int? categoryId)
+        public IQueryable<Product> GetProduct([QueryString("productID")] int? productId)
         {
             var _db = new WingtipToys.Models.ProductContext();
             IQueryable<Product> query = _db.Products;
-            if (categoryId.HasValue && categoryId > 0)
+            if (productId.HasValue && productId > 0)
             {
-                query = query.Where(p => p.CategoryID == categoryId);
+                query = query.Where(p => p.ProductID == productId);
+            }
+            else
+            {
+                query = null;
             }
             return query;
         }
